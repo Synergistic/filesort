36312d86533c529eab9c0b19f668577cdf0ff607
Synergistic <DFernandez226@gmail.com>
2014-02-06 22:30:13 -0500

    comments!

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 import shutil, os, os.path
 
+#create a list of the items in my target directory \new\
 files_to_sort = os.listdir('new')
-print files_to_sort
+
+#iterate through the items in the list from \new\
 for file in files_to_sort:
+
+	#pull out the files I want, ignored the folders, in this case .txt files
 	if '.txt' in file:
+	
+		#if its a letter...
 		if file[0].isalpha():
+		
+			#copy it to the letters directory with the same name
 			shutil.copy('new\\'+ str(file), 'letters\\' + str(file))
+			
+		#otherwise if its a number...
 		elif file[0].isdigit():
+		
+			#copy it to the numbers directory with the same name
 			shutil.copy('new\\'+ str(file), 'numbers\\' + str(file))
